bb7c29551bad5cc052c3413dc196517ca6c6b9cc
zezhong98 <873524892@qq.com>
2021-01-14 10:49:55 +0800

    lamp undebugged

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,6 +1,10 @@
 <?php
-  $pdo = new PDO('mysql:dbname=misc;host=localhost;port=3306', 'root', 'root');
-  $dbname = 'misc';
+  //  print_r(PDO::getAvailableDrivers());
+  $pdo = new PDO('mysql:dbname=Profilemng;host=localhost;port=3306', 'zzz', 'Zzz_123');
+  // alter user '%'@'%' identified by 'iii'
+  // set global validate_password.***=*;
+  // show variables like 'validate_password%'; 
+  $dbname = 'Profilemng';
 
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  ?>
